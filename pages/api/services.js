60d2c1566bc861b66ebe54900e7f7caaaef37add
Sir-Aguiar <felipeferreiraaguiar9@gmail.com>
2022-01-21 11:14:44 -0400

    API CORS

--- pages/api/services.js
+++ pages/api/services.js
@@ -11,14 +11,8 @@ async function Conectar() {
 }
 
 export default async function Timing(request, response) {
-
-  response.setHeader('Access-Control-Allow-Credentials', true)
+  
   response.setHeader('Access-Control-Allow-Origin', '*')
-  response.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  response.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
 
   const { db } = await Conectar()
   const users = await db.collection('users')
