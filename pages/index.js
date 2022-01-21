b795eedb9a1debdb60ff28a54a835d11838ff706
Sir-Aguiar <felipeferreiraaguiar9@gmail.com>
2022-01-21 16:15:55 -0400

    crossOrigin

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
       <Head>
         <title>{loged ? 'Home page' : 'Login Page'}</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Gemunu+Libre:wght@300&family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
       </Head>
 
