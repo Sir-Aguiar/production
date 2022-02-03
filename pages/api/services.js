61634d586c34a6d5f26f70159ffc005e4b7d4cd5
Felipe Ferreira Aguiar <felipeferreiraaguiar9@gmail.com>
2022-02-03 15:59:09 -0400

    Update services.js

--- pages/api/services.js
+++ pages/api/services.js
@@ -31,7 +31,6 @@ export default async function Timing(request, response) {
       return value;
     }
   }
-  response.setHeader('Access-Control-Allow-Origin', '*')
 
   const { service } = request.body
   async function ConnectToUserDB() {
